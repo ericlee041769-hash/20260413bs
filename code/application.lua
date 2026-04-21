ba9bc4ab853e2d60919f21568480d41901dfff52
Eric <liseng2001@163.com>
2026-04-21 15:32:02 +0800

    Wire SMS alarms into application loop

--- code/application.lua
+++ code/application.lua
@@ -3,6 +3,8 @@ local application = {}
 local app_config = require("app_config")
 local app_state = require("app_state")
 local app_collect = require("app_collect")
+local app_alarm = require("app_alarm")
+local app_sms = require("app_sms")
 local ggpio = require("ggpio")
 local gsht30 = require("gsht30")
 local gbaro = require("gbaro")
@@ -41,12 +43,22 @@ end
 local function start_collection_loop()
 	sys.taskInit(function()
 		local last_report_ms = 0
+		local alarm_runtime = {
+			active_map = {},
+			door_open_since_ms = nil
+		}
 
 		while true do
 			local cfg = app_config.get()
 			local snapshot = app_collect.collect_once()
+			local alarm = app_alarm.evaluate(cfg, snapshot, alarm_runtime, now_ms())
 
 			app_state.save_latest(snapshot)
+			alarm_runtime = type(alarm.runtime) == "table" and alarm.runtime or alarm_runtime
+
+			if alarm.should_send_sms == true then
+				app_sms.send_alert(cfg.alarm_sms_phone, alarm.sms_text)
+			end
 
 			if last_report_ms == 0 or (now_ms() - last_report_ms) >= cfg.report_interval_ms then
 				gmqtt.publish_snapshot(snapshot)
